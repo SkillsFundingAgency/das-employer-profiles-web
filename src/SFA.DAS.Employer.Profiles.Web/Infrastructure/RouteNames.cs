ca6108afaa08c104f20e184ecba272d8811808be
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2023-01-26 08:05:09 +0000

    FAI-725 - Add change sign in details view

--- src/SFA.DAS.Employer.Profiles.Web/Infrastructure/RouteNames.cs
+++ src/SFA.DAS.Employer.Profiles.Web/Infrastructure/RouteNames.cs
@@ -3,4 +3,5 @@ namespace SFA.DAS.Employer.Profiles.Web.Infrastructure;
 public static class RouteNames
 {
     public const string SignOut = "sign-out";
+    public const string ChangeSignInDetails = "change-sign-in-details";
 }
