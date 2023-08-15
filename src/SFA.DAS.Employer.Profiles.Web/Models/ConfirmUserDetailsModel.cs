a7f6feb84b22f434d20bb924d81176d3e8c9abb9
Corey <corey.faulconbridge@digital.education.gov.uk>
2023-08-15 15:08:19 +0100

    new model

---
+++ src/SFA.DAS.Employer.Profiles.Web/Models/ConfirmUserDetailsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.Employer.Profiles.Web.Models
+{
+    public class ConfirmUserDetailsModel : ViewModelBase
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string? CorrelationId { get; set; }
+    }
+}
