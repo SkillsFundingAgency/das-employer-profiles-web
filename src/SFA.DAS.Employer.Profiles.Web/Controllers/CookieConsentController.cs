80b250dd0190f424f9b329a0e53942946ec460dd
Jon Harris <jon.harris@fuitsu.com>
2025-02-28 12:03:21 +0000

    Adding cookie banner and associated bits

---
+++ src/SFA.DAS.Employer.Profiles.Web/Controllers/CookieConsentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.Employer.Profiles.Web.Controllers;
+
+public class CookieConsentController : Controller
+{
+    [HttpGet]
+    [Route("cookieConsent")]        
+    public IActionResult Settings()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    [Route("cookieConsent/details")]
+    public IActionResult Details()
+    {
+        return View();
+    }
+}
---
+++ src/SFA.DAS.Employer.Profiles.Web/Helpers/ControllerConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.Employer.Profiles.Web.Helpers;
+
+public static class ControllerConstants
+{
+    public const string CookieConsentControllerName = "CookieConsent";
+    public const string CookieConsentDetailsActionName = "Details";
+    public const string CookieConsentSettingsActionName = "Settings";
+}
