e5f664926d9561128d9d91c8a171e00cf12479e0
Jon Harris <jon.harris@fuitsu.com>
2025-03-03 07:57:50 +0000

    Removing unnecessary cookie bits

--- src/SFA.DAS.Employer.Profiles.Web/Helpers/ControllerConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SFA.DAS.Employer.Profiles.Web.Helpers;
-
-public static class ControllerConstants
-{
-    public const string CookieConsentControllerName = "CookieConsent";
-    public const string CookieConsentDetailsActionName = "Details";
-    public const string CookieConsentSettingsActionName = "Settings";
-}
